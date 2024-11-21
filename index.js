2113fe3059053d8dbc29f2b3d272bfd2a573d2bc
GiovanniTammaro9 <gio_play@live.it>
2024-11-21 14:23:54 +0100

    utilizzato il destructuring con valori predefiniti

--- index.js
+++ index.js
@@ -5,5 +5,14 @@ let [primo, _, terzo ] = numeri;
 console.log(primo);
 console.log(terzo);
 
+let arrNumeri = [10, 20, 30];
+//utilizzo destructuring con valori predefiniti
+let [first = 0, second = 0, third = 0, quarto = 0, quinto = 0] = arrNumeri;
+ console.log(first);
+ console.log(second);
+ console.log(third);
+ console.log(quarto);
+ console.log(quinto);
+
 
 
